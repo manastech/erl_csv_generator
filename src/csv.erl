cd72575f5187fde159caf9cd9eed70b3dfdc466c
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-04-01 13:32:43 -0300

    Initial commit

---
+++ src/csv.erl
@@ -0,0 +1,34 @@
+-module(csv).
+
+-export([newline/1, comma/1, field/2, row/2]).
+
+newline(File) ->
+  file:write(File, "\n").
+
+comma(File) ->
+  file:write(File, ",").
+
+field(File, Value) when is_binary(Value) ->
+  Match = binary:match(Value, [<<",">>, <<"\n">>, <<"\"">>]),
+  case Match of
+    nomatch ->
+      file:write(File, Value);
+    _ ->
+      file:write(File, "\""),
+      file:write(File, binary:replace(Value, <<"\"">>, <<"\"\"">>)),
+      file:write(File, "\"")
+  end;
+field(File, Value) when is_list(Value) ->
+  field(File, list_to_binary(Value));
+field(File, Value) when is_integer(Value) ->
+  file:write(File, integer_to_list(Value)).
+
+row(File, []) ->
+  newline(File);
+row(File, [Value]) ->
+  field(File, Value),
+  newline(File);
+row(File, [Value | Rest]) ->
+  field(File, Value),
+  comma(File),
+  row(File, Rest).
