b29598e08695bab8f45ea762a252ea29c5c2153a
Mikhail Khotko <mkhotko@debian>
2016-10-06 11:26:13 +0500

    Add test case for unicode

--- test/csv_gen_tests.erl
+++ test/csv_gen_tests.erl
@@ -5,11 +5,13 @@ write_csv_test() ->
   {ok, File} = file:open("temp.csv", [write]),
   csv_gen:row(File, ["Header 1", "Header 2"]),
   csv_gen:row(File, [1, "hello", <<"good bye">>, "with \" quote","with \n newline", "with , comma", 2.0, "and the atom", hydrogen]),
+  csv_gen:row(File, ["and the unicode", [1084,1072,1084,1072], "that means mother in russian"]),
   file:close(File),
 
   {ok, Contents} = file:read_file("temp.csv"),
   io:format("wrote ~p~n",[Contents]),
-  ?assertEqual(<<"Header 1,Header 2\n1,hello,good bye,\"with \"\" quote\",\"with \n newline\",\"with , comma\",2.000000,and the atom,hydrogen\n">>, Contents).
+  ?assertEqual(<<"Header 1,Header 2\n1,hello,good bye,\"with \"\" quote\",\"with \n newline\",\"with , comma\",2.000000,and the atom,hydrogen\n"
+                 "and the unicode,",208,188,208,176,208,188,208,176,",that means mother in russian\n">>, Contents).
 
 double_quote_test() ->
   {ok, File} = file:open("temp.csv", [write]),
